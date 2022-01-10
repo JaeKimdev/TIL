4561c33c2258a46c0c25e274adbf30365f733333
Jae Hyung Kim <smfc.mene@gmail.com>
2022-01-10 22:06:30 +0800

    Q1000.A+B

---
+++ baekjoon/1000~1999/1000.py
@@ -0,0 +1,10 @@
+# link: https://www.acmicpc.net/problem/1000
+
+# Q: Enter the two integers A and B, and then outputs A+B.
+
+A, B = input().split()	# 입력되는 문자를 input()함수로 입력받고 split()함수로 나누어 A,B 변수에 저장
+print(int(A)+int(B))	# int() 함수로 A와 B를 정수로 변환 하고 두수의 합을 출력
+
+# same as below
+# A, B = map(int, input().split())
+# print(A+B)
