7392f7c9d8e3ead4b4907812c1c2513a2ab01fc3
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-01-22 11:34:19 +0800

    TIL-2022-01-22
    learn JS

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,42 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1);
-
 function handleTitleClick() {
-    h1.style.color = "blue";
-}
-
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!"
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue"){
+        newColor = "tomato";
+    }else{
+        newColor = "blue";
     }
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!"
-    }
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
+    h1.style.color = newColor
 }
-
-function handleWindowCopy() {
-    alert("Copyer!");
-}
-
-function handleWindowOffline() {
-    alert("SOS! no wifi!");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD");
-}
-
 
 h1.addEventListener("click", handleTitleClick);
-// same as h1.onclick = handleTitleClick;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("offline", handleWindowOnline);
