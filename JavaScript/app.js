679af1374f163f5d44d951a00f95a9e15449cbd4
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-01-22 12:09:03 +0800

    TIL-2022-01-22
    refactor: use toggle

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,12 +1,7 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const clickedCalss = "clicked"
-    if(h1.classList.contains(clickedCalss)){
-        h1.classList.remove(clickedCalss);
-    } else {
-        h1.classList.add(clickedCalss);
-    }
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
