a345f1446b0fae9b912a8d2ddce1b37b019f8b67
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-02-03 15:16:05 +0800

    Binary2Decimal
    Java program that accepts a binary number and converts it into the decimal equivalent.

---
+++ Java/Binary2Decimal/Binary2Decimal.java
@@ -0,0 +1,40 @@
+package Binary2Decimal;
+
+import java.util.Scanner;
+
+public class Binary2Decimal {
+
+    public static void main(String[] args) {
+
+// solve 1. Use Integer.parseInt         
+//        Scanner sc = new Scanner(System.in);
+//        int binary;
+//        String binaryString;
+//
+//        System.out.print("Enter a binary number (0~11111): ");
+//        binaryString = sc.nextLine();
+//
+//        int decimal = Integer.parseInt(binaryString, 2);
+//        System.out.println(binaryString + "(2) is " + decimal + "(10)");
+//
+//
+// solve 2. Make a logic
+        Scanner sc = new Scanner(System.in);
+        int binary;
+        int n = 0;
+        int decimal = 0;
+
+        System.out.print("Enter a binary number (0~11111): ");
+        binary = sc.nextInt();
+        int binaryResult = binary;
+
+        while (binary != 0) {
+            int temp = binary % 10;
+            decimal += temp * Math.pow(2, n);
+            binary = binary / 10;
+            n++;
+        }
+        System.out.println(binaryResult + "(2) is " + decimal + "(10)");
+    }
+
+}
