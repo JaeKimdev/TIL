e80661337073c044c672acc8864c67988818bb09
Jae Hyung Kim <84453688+mene79@users.noreply.github.com>
2022-02-04 22:24:05 +0800

    FileReadWrite

---
+++ Java/FileReadWrite/FileWrite.java
@@ -0,0 +1,22 @@
+import java.nio.file.*;
+import java.io.*;
+
+import static java.nio.file.StandardOpenOption.*;
+
+public class FileWrite {
+    public static void main(String[] args) {
+        Path file = Paths.get("message.txt");
+        String s = "This is a test message to save file";
+        byte[] data = s.getBytes();
+        OutputStream output = null;
+
+        try {
+            output = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
+            output.write(data);
+            output.flush();
+            output.close();
+        } catch (Exception e) {
+            System.out.println("Message: " + e);
+        }
+    }
+}
