056b7800e900bf459ef81c3b5703540026d7cc28
Jae Hyung Kim <smfc.mene@gmail.com>
2022-03-10 21:08:16 +0800

    TIL-2022-03-10

---
+++ Python/ex02.py
@@ -0,0 +1,12 @@
+# The question of receiving the number of participants and obtaining the number of necessary foods.
+# 1 chicken per person, 3 beer per person. 2 pieces of cake.
+
+number = int(input("Please input number of participants: "))
+
+chickens = number
+beers = number * 3
+cakes = number * 2
+
+print("Need order Chicken: ", chickens)
+print("Need order beers: ", beers)
+print("Need order cakes: ", cakes)
