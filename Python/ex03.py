4c951d8952342cf849ad79643cd7796afb874d91
Jae Hyung Kim <smfc.mene@gmail.com>
2022-03-10 21:11:27 +0800

    TIL-2022-03-10

---
+++ Python/ex03.py
@@ -0,0 +1,8 @@
+# A program that receives two integers from a user and calculates the sum of the two numbers.
+
+x = int(input("Please input 1st integer: "))
+y = int(input("Please input 2nd integer: "))
+
+sum = x + y
+
+print("sum of 2 numbers: ", sum)
