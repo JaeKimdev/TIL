fa7feabfe07fd6b2e3adcaf7138ad6f39c6ac95d
Jae Hyung Kim <smfc.mene@gmail.com>
2022-03-10 21:01:05 +0800

    TIL-2022-03-10
    Example of swapping two numbers.

---
+++ Python/ex01.py
@@ -0,0 +1,12 @@
+# Example of swapping two numbers.
+
+num1 = 100
+num2 = 200
+
+print("num1: " , num1 , ", num2: " , num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("num1: " , num1 , ", num2: " , num2)
