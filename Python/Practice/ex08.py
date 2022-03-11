aec60155e3d179543ae20ed82c5ae44f4433ebe9
Jae Hyung Kim <smfc.mene@gmail.com>
2022-03-11 13:25:05 +0800

    TIL-2022-03-11
    math function

---
+++ Python/Practice/ex08.py
@@ -0,0 +1,23 @@
+# Using built-in functions and math library functions
+
+from math import *
+
+# Absolute math value
+print(abs(-77))
+print(abs(77))
+
+# Rounded up math
+print(round(1.222))
+print(round(1.522))
+
+# Find the maximum value out of the given values.
+print(max(10, -20, 100, 999))
+
+# Find the minimum value out of the given values.
+print(min(10, -20, 100, 999))
+
+# Finding the square root
+print(sqrt(4.0))
+
+# same as **
+print(pow(3, 2))
