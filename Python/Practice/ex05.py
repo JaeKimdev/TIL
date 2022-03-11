2a9f8d607d0084238eb05e5c26eddc92f053363c
Jae Hyung Kim <smfc.mene@gmail.com>
2022-03-11 13:02:29 +0800

    TIL-2022-03-11
    Operator

---
+++ Python/Practice/ex05.py
@@ -0,0 +1,15 @@
+# Operator
+
+print(3 +4 )
+print(3.14 * 5.0 * 5.0)
+print(1 + (4 * 8) - (10 / 2))
+
+# If you write down `//`, they are output as integer type.
+
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+
+# Use ** to calculate the square
+
+print(3 ** 3)
