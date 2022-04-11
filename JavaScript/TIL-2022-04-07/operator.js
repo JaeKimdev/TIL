0bcadb5bb1fb1491d83e43b3041652eb771a82f0
Jae Hyung Kim <smfc.mene@gmail.com>
2022-04-11 16:11:35 +0800

    TIL-2022-04-11
    operator

--- JavaScript/TIL-2022-04-07/operator.js
+++ JavaScript/TIL-2022-04-07/operator.js
@@ -91,3 +91,86 @@ console.log("" == false);
 console.log("" === false);
 console.log(null == undefined);
 console.log(null === undefined);
+
+// 8. Conditional operators: if
+// if, else if, else
+const name = "Jae";
+if (name === "Jae") {
+  console.log("Hello Jae!");
+} else if (name === "coder") {
+  console.log("Hello coder!");
+} else {
+  console.log("unknown");
+}
+
+// 9. Ternary operator
+// condition ? value1 : value2;
+console.log(name === "Jae" ? "yes" : "no");
+
+// 10. Switch statment
+// use for multiple if checks
+// use for enum-like value check
+// use for multiple type checks in TS
+const browser = "IE";
+switch (browser) {
+  case "IE":
+    console.log("go away!");
+    break;
+  case "Chrome":
+  case "Firefox":
+    console.log("cool!");
+    break;
+  default:
+    console.log("same all around!");
+    break;
+}
+
+// 11. Loops
+// while loop, while the condition is true
+// body code is executed
+let i = 3;
+while (i > 0) {
+  console.log(`while: ${i}`);
+  i--;
+}
+
+// do while loop, body code is executed first,
+// then check condition
+do {
+  console.log(`do while: ${i}`);
+  i--;
+} while (i > 0);
+
+//for loop, for(begin; condition; step)
+for (i = 3; i > 0; i--) {
+  console.log(`for: ${i}`);
+}
+
+for (let i = 3; i > 0; i = i - 2) {
+  //inline variable declaration
+  console.log(`inline variable for: ${i}`);
+}
+
+// nested loops
+for (let i = 0; i < 10; i++) {
+  for (let j = 0; j < 10; j++) {
+    console.log(`${i}, ${j}`);
+  }
+}
+
+// break, continue
+// iterate from 0 to 10 and print only even numbers ( use continue)
+for (let i = 0; i < 11; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
+
+// iterate from 0 to 10 and print numbers until reaching 8 (use break)
+for (let i = 0; i < 11; i++) {
+  if (i > 8) {
+    break;
+  }
+  console.log(i);
+}
