63c7be99c2b3f36690715e19ee497fdb21035e35
Jae Hyung Kim <smfc.mene@gmail.com>
2022-04-22 10:35:53 +0800

    TIL-2022-04-22
    JSON
    JSON Diff checker: http://www.jsondiff.com/
    JSON Beautifier/editor: https://jsonbeautifier.org/
    JSON Parser: https://jsonparser.org/
    JSON Validator: https://tools.learningcontainer.com/json-validator/

---
+++ JavaScript/js summary/json.js
@@ -0,0 +1,47 @@
+// JSON
+// JavaScript Object Nortation
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(["apple", "banana"]);
+console.log(json);
+
+const rabbit = {
+  name: "tori",
+  color: "white",
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${name} can jump!`);
+  },
+};
+
+json = JSON.stringify(rabbit); // 함수는 오브젝트에 들어있는게 아니기 때문에 제외된다
+console.log(json);
+
+json = JSON.stringify(rabbit, ["name", "color"]); // replacer: 이름과 색상만 나오게 하기
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === "name" ? "Jae" : value;
+});
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key}, value: ${value}`);
+  return key === "birthDate" ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump(); //error - due to obj is not a function
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
