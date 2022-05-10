8b26b68136f6aa27bd6bba120134ea09721c00ff
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-10 20:33:22 +0800

    Variables
    MDN variable: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Variables
    
    Lexical grammar: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#keywords

---
+++ JavaScript/js_summary/2.naming.js
@@ -0,0 +1,29 @@
+// Single line Commnet
+
+/**
+ * Multi line comment
+ *
+ */
+
+/**
+ * Syntax
+ * Latin letters(0-9, a-z, A-Z) and underscores(_) are allowed.
+ * Case insensitive.
+ * camelCase(first letter is lowercase)
+ */
+
+let apple;
+let redAplle;
+
+//Bad naming
+let number = 20;
+let audio1;
+let audio2;
+
+//Good naming
+let myAge = 20;
+let backgroundAudio;
+let windAudio;
+
+let audioBackground;
+let audioWind;
