fa7e8cf8470f431af71e6f942ecaf3c98c148936
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-10 21:05:57 +0800

    Create 5.boolean.js
    MDN Boolean : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Boolean

---
+++ JavaScript/js_summary/5.boolean.js
@@ -0,0 +1,26 @@
+let booleanTrue = true;
+let booleanFalse = false;
+console.log(booleanTrue);
+console.log(booleanFalse);
+
+// example:
+let isFree = true;
+let isActivated = false;
+let isEnrolled = true;
+console.log(isActivated);
+console.clear();
+
+// Falshy values - 거짓인 값:
+console.log(!!0); // !!를 붙이면 참/거짓을 반환한다.
+console.log(!!-0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+// Truthy values - 참인 값:
+console.log(!!1);
+console.log(!!-1);
+console.log(!!"text");
+console.log(!!{});
+console.log(!!Infinity);
