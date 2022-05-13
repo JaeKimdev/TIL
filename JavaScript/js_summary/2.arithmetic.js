b0094e79ab7e6662d1fbce2170589daa5e8165ee
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-13 23:08:50 +0800

    operators
    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference

---
+++ JavaScript/js_summary/2.arithmetic.js
@@ -0,0 +1,20 @@
+// Arithmetic operaters (산술 연산자)
+// +
+// -
+// *
+// -
+// *
+
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(Math.pow(5, 2));
+console.log(5 ** 2); // es7
+
+// + 연산자 주의점
+let text = "Hello" + "World";
+console.log(text);
+text = "1" + 2; // 숫자와 문자열을 더하면 문자열로 반환됨
+console.log(text);
