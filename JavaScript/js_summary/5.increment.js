36f98567944d0a913fdfd5065c6d4050396c2249
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-14 23:06:54 +0800

    Create 5.increment.js

---
+++ JavaScript/js_summary/5.increment.js
@@ -0,0 +1,26 @@
+// Increment & decrement Operators (증가 & 감소 연산자)
+let a = 0;
+console.log(a);
+a++; // a = a + 1;
+console.log(a);
+a--; // a = a - 1;
+console.log(a);
+
+console.clear();
+// a++ 필요한 연산을 하고, 그 뒤 값을 증가시킴
+// ++a 값을 먼저 증가하고, 필요한 연산을 함
+
+a = 0;
+let b = a++;
+console.log(b);
+console.log(a);
+
+c = 0;
+let d = ++c;
+console.log(d);
+console.log(c);
+
+e = 0;
+console.log(e++);
+f = 0;
+console.log(++f);
