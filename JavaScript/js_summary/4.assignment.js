db38dde13a7214576e32c5caabc9f1c1aab6ef43
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-14 23:00:38 +0800

    Create 4.assignment.js

---
+++ JavaScript/js_summary/4.assignment.js
@@ -0,0 +1,18 @@
+// Assignment Operators (할당 연산자)
+let a = 1;
+a = a + 2;
+console.log(a);
+
+a += 2;
+console.log(a);
+
+a -= 2;
+console.log(a);
+
+a *= 2;
+console.log(a);
+
+a /= 2;
+a %= 2;
+a **= 2;
+console.log(a);
