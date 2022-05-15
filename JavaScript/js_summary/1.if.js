0cec3c5f16cc51e12fb612a2b29dbaaeae0e4a7b
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-15 23:07:05 +0800

    Create 1.if.js

---
+++ JavaScript/js_summary/1.if.js
@@ -0,0 +1,12 @@
+// Conditional Statement
+// if(condition) { }
+// if(condition) { } else { }
+// if(condition 1) { } else if(condition 2) { } else { }
+let fruit = "apple";
+if (fruit === "apple") {
+  console.log("apple");
+} else if (fruit === "banana") {
+  console.log("banana");
+} else {
+  console.log("other");
+}
