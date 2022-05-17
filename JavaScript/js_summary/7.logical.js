8527b175a02b09dde2289bde58555cc67ad6c6c4
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-17 23:08:51 +0800

    Create 7.logical.js

---
+++ JavaScript/js_summary/7.logical.js
@@ -0,0 +1,31 @@
+// Logical Operators
+// && - and
+// || - or
+// ! - not
+// !! - double not
+
+let num = 8;
+if (num >= 0 && num < 9) {
+  console.log("👍");
+}
+
+if (num >= 0 || num > 20) {
+  console.log("👍👍");
+}
+
+if (num != 8) {
+  console.log("👍👍👍");
+}
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log(!"text"); // false
+console.log(!!"text"); // true
