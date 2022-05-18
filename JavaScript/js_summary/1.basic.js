2454a1e9140780a44253b516553125b0f184eec2
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-18 23:31:55 +0800

    Create 1.basic.js
    MDN function : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function

---
+++ JavaScript/js_summary/1.basic.js
@@ -0,0 +1,20 @@
+// example 1
+function add(num1, num2) {
+  return num1 + num2;
+}
+const result = add(1, 2);
+console.log(result);
+
+// example 2
+
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`;
+}
+
+let lastName = "Kim";
+let firstName = "Jae";
+console.log(fullName(firstName, lastName));
+
+let lastName2 = "Watson";
+let firstName2 = "Kyle";
+console.log(fullName(firstName2, lastName2));
