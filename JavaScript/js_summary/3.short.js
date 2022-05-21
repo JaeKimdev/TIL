0be12f6ad59afd4518cba85023da7d27aec55f49
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-21 22:43:21 +0800

    Create 3.short.js

---
+++ JavaScript/js_summary/3.short.js
@@ -0,0 +1,9 @@
+const x = 0;
+const y = 0;
+const coordiante = { x, y }; // same as { x: x, y: y };
+console.log(coordiante);
+
+function makeObj(name, age) {
+  return { name, age };
+  // same as return { name: name, age: age };
+}
