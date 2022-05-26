0221f12882b85b831196571b6cac23fe1b804db0
Jae Hyung Kim <smfc.mene@gmail.com>
2022-05-26 23:21:41 +0800

    ABCLoan

---
+++ Java/ABCLoan/dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"myloan","l":"BusinessLoan"},{"p":"createloans","l":"CreateLoans"},{"p":"myloan","l":"Loan"},{"p":"myloan","l":"LoanConstants"},{"p":"myloan","l":"PersonalLoan"}];updateSearchResults();
---
+++ Java/CustomerManagement/src/CustomerManagement.java
@@ -0,0 +1,6 @@
+public class CustomerManagement {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
