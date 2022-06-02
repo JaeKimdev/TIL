0d52689ad41bc9788c0e0fab265bf4a588e6a353
Jae Hyung Kim <84453688+JaeKimdev@users.noreply.github.com>
2022-06-02 23:12:34 +0800

    Add files via upload

---
+++ mySQL/NRL League/Models/Metadata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AT3.Models
+{
+    public class teamMetadata
+    {
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public string City;
+    }
+
+    public class gameMetadata
+    {
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public string HostTeam;
+    }
+
+    public class ladderMetadata
+    {
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public string TeamName;
+    }
+}
---
+++ mySQL/NRL League/Models/PartialClasses.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace AT3.Models
+{
+    [MetadataType(typeof(teamMetadata))]
+    public partial class Teams
+    { }
+
+    [MetadataType(typeof(gameMetadata))]
+    public partial class Games
+    { }
+
+    [MetadataType(typeof(ladderMetadata))]
+    public partial class Ladder
+    { }
+}
