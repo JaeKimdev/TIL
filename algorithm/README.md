378f942300bdce279c973bca6094abd48fed053c
Jae Hyung Kim <84453688+JaeKimdev@users.noreply.github.com>
2022-10-10 16:27:19 +0800

    Delete algorithm directory

--- algorithm/README.md
+++
@@ -1,9 +0,0 @@
-# Swift Algorithm
-
-## LeetCode - Easy
-
-|Number|solution|
-|:--:|:--:|
-|[1. Two Sum](https://leetcode.com/problems/two-sum/)|[link]()|
-
-
--- algorithm/programmers/220109 - Python/221009.py
+++
@@ -1,7 +0,0 @@
-def solution(s):
-    e = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-    for i in range(10):
-        # s.replace(a, b): 문자열 s에서 a를 찾아 b로 바꾼다
-        s = s.replace(e[i], str(i))
-    return int(s)
-
