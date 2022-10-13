1724098a0c46c58a76a0ef3542c626efd06f8da0
Jae Hyung Kim <84453688+JaeKimdev@users.noreply.github.com>
2022-10-13 21:50:05 +0800

    Add files via upload

---
+++ dynamicScrollView/practiceUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practiceUIKit
+//
+//  Created by smfc on 4/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
+
+
+
